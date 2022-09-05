4340fb9dce1807c5d0ff188036fd063337eb8395
Sergey Shlyakhov <sergey.schlyakhov@ya.ru>
2022-09-05 10:44:22 +0300

    Init

---
+++ src/test/java/com/intro/IntroductionREST/IntroductionRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intro.IntroductionREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
