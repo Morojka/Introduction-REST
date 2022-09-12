cdfb167ec08eb7ed046de2d426b5a1cbc23fd7ae
Sergey Shlyakhov <shlyahov@axiomatika.ru>
2022-09-12 09:40:41 +0300

    Revert "Merge branch 'release/1.0'"
    This reverts commit c772fc0f66da781f047aebe2f37de1649dc3f0a9, reversing
    changes made to dce52922717aa1b0be946f2919d8ca68f69bc74f.

--- src/main/java/com/intro/IntroductionREST/controller/RequestController.java
+++ src/main/java/com/intro/IntroductionREST/controller/RequestController.java
@@ -6,11 +6,7 @@ import com.intro.IntroductionREST.repository.PersonRepository;
 import org.json.JSONObject;
 import org.json.XML;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 public class RequestController {
@@ -20,39 +16,24 @@ public class RequestController {
     @Autowired
     PersonClient personClient;
 
-    Logger logger = LoggerFactory.getLogger(RequestController.class);
-
     @PostMapping(value = "/new-request", produces = "application/xml")
     @ResponseBody
     public String processRequest(@RequestBody Person person) throws Exception {
         //преобразуем json-запрос в xml через модель Person
         String personObject = XML.toString(new JSONObject(person), Person.class.getSimpleName().toLowerCase());
 
-        logger.info("Person in request: \n" + person.toString());
-
         try {
             personRepository.save(person);
-            logger.info("Successfully saved person.");
         } catch (Exception e) {
-            logger.error("Could not save person from request: \n" + person.toString());
             throw new Exception("There was an error during person creation process. Check your credentials and try again!");
         }
-
-        logger.info("Sending xml representation of person to soap-application: \n" + person.toString());
 
         //отправка запроса в soap-приложение
         String response = personClient.processPerson(personObject);
 
         //преобразуем ответ soap-приложения в модель Person и сохраняем в БД
         personRepository.save(personClient.mapPersonFromXML(response));
-        logger.info("Successfully saved person from response: \n" + response);
 
         return response;
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex)
-    {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
